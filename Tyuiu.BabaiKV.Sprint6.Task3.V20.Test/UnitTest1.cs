bc4dfea6e7194f64b586628e52d021a6786787da
Tyuiu.BabaiKV@outlook.com <Tyuiu.BabaiKV@outlook.com>
2024-12-10 15:35:52 +0500

    Task3

---
+++ Tyuiu.BabaiKV.Sprint6.Task3.V20.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.BabaiKV.Sprint6.Task3.V20.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
